095b66acf9ca0c251a8398bc340620296e6fe0fc
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-04-21 21:12:47 -0400

    Add fromJson method to Translator for translating a whole JSON object at once

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var Translator = {
     Strings: require('./translators/StringsTranslator.js'),
     Terrain: require('./translators/TerrainTranslator.js'),
     Units: require('./translators/UnitsTranslator.js'),
+    
+    fromJson: function(mapJson, outDir) {
+        // Translate all JSON...
+        if(mapJson.doodads) {} // translate
+        // ...
+    }
 };
 
 module.exports = Translator;
