aac55e81a5ee497fb1d79cb078eab861eaa0595a
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-04-21 23:29:31 -0400

    Clean up Terrain translator

--- translators/TerrainTranslator.js
+++ translators/TerrainTranslator.js
@@ -11,8 +11,8 @@ var TerrainTranslator = function(terrainJson, outputPath) {
      */
     outBuffer.addString('W3E!'); // file id
     outBuffer.addInt(11); // file version
-    outBuffer.addChar(terrainJson.tileset); // subversion 0x0B
-    outBuffer.addInt(+terrainJson.customtileset); // num of trees
+    outBuffer.addChar(terrainJson.tileset); // base tileset
+    outBuffer.addInt(+terrainJson.customtileset); // 1 = using custom tileset, 0 = not
         
     
     /*
@@ -38,6 +38,9 @@ var TerrainTranslator = function(terrainJson, outputPath) {
      */
     outBuffer.addInt(terrainJson.map.width);
     outBuffer.addInt(terrainJson.map.height);
+    
+    // Unsupported
+    // map.offset is not implemented yet.. so we hardcode
     outBuffer.addByte(0x00);outBuffer.addByte(0x00);outBuffer.addByte(0x80);outBuffer.addByte(0xc5);
     outBuffer.addByte(0x00);outBuffer.addByte(0x00);outBuffer.addByte(0x80);outBuffer.addByte(0xc5);
     //outBuffer.addFloat(terrainJson.map.offset.x);
@@ -50,19 +53,11 @@ var TerrainTranslator = function(terrainJson, outputPath) {
     for(var i = terrainJson.tiles.length - 1; i >= 0; i--) {
         // write each for of tiles
         terrainJson.tiles[i].forEach(function(tile) {
-            // TODO: below is test code to try out tiles
-            outBuffer.addShort(tile[0]); // height
-            outBuffer.addShort(tile[1]); // water
-            outBuffer.addByte(tile[2]+''+tile[3]); // flags/tile
-            outBuffer.addByte(tile[4]); // texture details
-            outBuffer.addByte(tile[5]+''+tile[6]); // cliff tile/layer
-            
-            
-            /* outBuffer.addShort(8192);
-            outBuffer.addShort(0x2000);
-            outBuffer.addByte(0x00);
-            outBuffer.addByte(0x04);
-            outBuffer.addByte(0xf2); */
+            outBuffer.addShort(tile[0]);            // height
+            outBuffer.addShort(tile[1]);            // water
+            outBuffer.addByte(tile[2]+''+tile[3]);  // flags/tile
+            outBuffer.addByte(tile[4]);             // texture details
+            outBuffer.addByte(tile[5]+''+tile[6]);  // cliff tile/layer
         });
     }
     
