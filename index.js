373a624eaa6ef2507836a758088479eea874db40
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-04-22 14:52:57 -0400

    Remove fromJson from Translator
    The Translator module should only expose the translators. It should not convert an entire map JSON to individual files, because that is Ice Sickle's purpose - the map format is defined there, and should not be assumed here.

--- index.js
+++ index.js
@@ -3,31 +3,7 @@ var Translator = {
     Doodads: require('./translators/DoodadsTranslator.js'),
     Strings: require('./translators/StringsTranslator.js'),
     Terrain: require('./translators/TerrainTranslator.js'),
-    Units: require('./translators/UnitsTranslator.js'),
-    
-    fromJson: function(mapJson, outDir) {
-        
-        if(mapJson.doodads) {
-            var doodadsTranslator = new Translator.Doodads(mapJson.doodads, outDir);
-            doodadsTranslator.write();
-        }
-        
-        if(mapJson.strings) {
-            var stringsTranslator = new Translator.Strings(mapJson.strings, outDir);
-            stringsTranslator.write();
-        }
-        
-        if(mapJson.terrain) {
-            var terrainTranslator = new Translator.Terrain(mapJson.terrain, outDir);
-            terrainTranslator.write();
-        }
-        
-        if(mapJson.units) {
-            var unitsTranslator = new Translator.Units(mapJson.units, outDir);
-            unitsTranslator.write();
-        }
-        
-    }
+    Units: require('./translators/UnitsTranslator.js')
 };
 
 module.exports = Translator;
