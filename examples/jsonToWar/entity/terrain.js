6b37dce6ab047e5556cee152f08658e2388c44ad
ChiefOfGxBxL <langgb@clarkson.edu>
2019-06-09 23:34:59 -0400

    Add warToJson example for terrain, update various example files for terrain translator

--- examples/jsonToWar/entity/terrain.js
+++ examples/jsonToWar/entity/terrain.js
@@ -4,29 +4,10 @@
  * instead of the first. That is, when using wc3maptranslator in your code,
  * call `require('wc3maptranslator')` instead of `require('../../index.js')`.
  */
- const Translator = require('../../../index.js'); // require('wc3maptranslator');
- const { WarFile, Write } = require('../writeHelper.js');
+const Translator = require('../../../index.js'); // require('wc3maptranslator');
+const { WarFile, Write } = require('../writeHelper.js');
 
-// Place a few regions on the map
-const data = {
-    "tileset": "L",
-    "customtileset": true,
-    "tilepalette": ["LDrt"],
-    "clifftilepalette": [""],
-    "map": {
-        "width": 32,
-        "height": 32
-    },
-    "tiles": [
-        [
-            [0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0],
-            [2, 0, 0, 0, 0, 0, 0],
-            [3, 0, 0, 0, 0, 0, 0],
-            [4, 0, 0, 0, 0, 0, 0]
-        ]
-    ]
-};
+const terrainData = require('../input/terrain.json');
 
-var terrainResult = new Translator.Terrain.jsonToWar(data);
+var terrainResult = new Translator.Terrain.jsonToWar(terrainData);
 Write(WarFile.Entity.Terrain, terrainResult.buffer);
