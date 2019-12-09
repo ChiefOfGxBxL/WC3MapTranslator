007ac57bb4cdd281647403a855a92fcf89e0358a
rufreakde <rc@dev-six.com>
2019-12-09 19:54:29 +0100

    #1

--- lib/HexBuffer.ts
+++ lib/HexBuffer.ts
@@ -1,6 +1,6 @@
 const ieee754 = require('ieee754'),
     IntN = require('intn'),
-    intToHex = function (intV, isShort) {
+    intToHex = function (intV: number, isShort: number) {
         // Creates a new 32-bit integer from the given number
         let intSize = isShort ? 16 : 32,
             intNSize = new IntN(intSize),
@@ -12,7 +12,7 @@ const ieee754 = require('ieee754'),
             return '0x' + Byte.toString(16);
         });
     },
-    charToHex = function (character) {
+    charToHex = function (character: string) {
         return '0x' + character.charCodeAt(0).toString(16);
     };
 
